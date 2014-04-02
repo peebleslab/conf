90c4d2d4ff017aa60f023fe327d6e66c2d928b59
Dave Kilian <dave.kilian@gmail.com>
2014-04-02 01:40:01 +0000

    Package installation and /www subdir creation

--- peebleslab.pp
+++ peebleslab.pp
@@ -8,17 +8,6 @@ $www_dir    = '/www'
 $www_user   = 'plab'
 $www_group  = 'plab'
 
-# Create the www directory, which will contain all site content
-#
-file { $www_dir:
-    ensure  => directory,
-    owner   => $www_user,
-    group   => 'root',
-    mode    => 2575,
-
-    require => User[$www_user]
-}
-
 # Create the www user, who will be responsbile for serving all site content.
 # For security reasons, this user will have restricted access to the machine
 # and the sites it runs.
@@ -37,32 +26,56 @@ group { $www_group:
 }
 
 # Install required packages
+#
+package { 'python':
+    ensure => present
+}
 
-# TODO install python
-# TODO install pip
-# TODO install virtualenv using pip the source
-# TODO install varnish
+package { 'python-pip':
+    ensure  => present,
+    require => Package['python']
+}
+
+package { 'virtualenv':
+    ensure   => present,
+    provider => 'pip',
+    require  => Package['python-pip']
+}
+
+package { 'varnish':
+    ensure => present
+}
 
 # Create the www site hierarchy
+#
+file { [ 
+    "$www_dir/dev-data", "$www_dir/prod-data",
+    "$www_dir/dev-main", "$www_dir/prod-main",
+    "$www_dir/dev-store", "$www_dir/prod-store",
+    "$www_dir/dev-admin", "$www_dir/prod-admin",
+    "$www_dir/dev-vault", "$www_dir/prod-vault"
+    ]:
 
-# TODO prod-data / dev-data
-# TODO prod-main / dev-main
-# TODO prod-store / dev-store
-# TODO prod-admin / dev-admin
-# TODO prod-vault / dev-vault
+    ensure  => directory,
+    owner   => $www_user,
+    group   => 'root',
+    mode    => 2575,
+
+    require => User[$www_user]
+}
 
 # Populate the sites
-
+#
 # TODO clone each site from git
 # TODO run each site's setup script (which creates a venv and installs things)
 # TODO download secrets.py (which has API secrets / password hashes / etc)
 
 # Configure the system
-
+#
 # TODO download and install the varnish configuration
 # TODO download and install init.d scripts for each www site
 
 # Bring up the sites
-
+#
 # TODO
 
